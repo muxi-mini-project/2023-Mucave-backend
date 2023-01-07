54f701ce14fddd86c70ea43d492e63fcab693017
big-dust <115332044+big-dust@users.noreply.github.com>
2023-01-07 22:05:07 +0800

    Update whether_my_post.go

--- service/whether_my_post.go
+++ service/whether_my_post.go
@@ -1,16 +1,1 @@
-package service
 
-import (
-	"Mucave/model"
-	"strconv"
-)
-
-func WhetherMyPost(userId interface{}, postId interface{}) bool {
-	posts, _ := model.QueryOneUserPosts(userId)
-	for _, post := range posts {
-		if strconv.Itoa(int(post.ID)) == postId {
-			return true
-		}
-	}
-	return false
-}
